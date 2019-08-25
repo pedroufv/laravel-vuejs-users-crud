a748740d298035a72301ed8d83189e98da1d23ee
Pedro Augusto <pams.pedro@gmail.com>
2019-08-25 09:48:42 -0300

    show list of users with pagination using laravel resource

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,36 @@
-/**
- * First, we will load all of this project's Javascript utilities and other
- * dependencies. Then, we will be ready to develop a robust and powerful
- * application frontend using useful Laravel and JavaScript libraries.
- */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-require('./bootstrap');
+Vue.use(VueRouter);
+
+import App from './views/App'
+import Hello from './views/Hello'
+import Home from './views/Home'
+import UsersIndex from './views/UsersIndex';
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+        {
+            path: '/users',
+            name: 'users.index',
+            component: UsersIndex,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
