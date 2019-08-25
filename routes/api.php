ffbd349fd9f864009ecef5d4a8977bf5c1c3c99a
Pedro Augusto <pams.pedro@gmail.com>
2019-08-25 10:36:01 -0300

    do the users' edit

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,6 @@
 
 Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
+    Route::get('/users/{user}', 'UsersController@show');
+    Route::put('/users/{user}', 'UsersController@update');
 });
